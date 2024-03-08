26ec2c2fefe2fbe86d11bcda8e52cebf81845422
Abel Mehedinteanu <111386807+abel325@users.noreply.github.com>
2024-03-08 09:08:22 +0200

    create Content component

---
+++ travel-journal-ui/src/components/Content/Content.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+import "./index.css"
+
+const Content = () => {
+    return (
+        <div id="content">
+            <Outlet />
+        </div>
+    );
+}
+
+export default Content;
