3b7204cf36e506692907fb16c30be3fc1cd1b838
Claudita8 <claudiaolaru2002@gmail.com>
2024-03-11 15:47:42 +0200

    NewTravelButton

--- travel-journal-ui/src/components/Home/Home.jsx
+++ travel-journal-ui/src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
+import React from 'react';
+import NewTravel from "../NewTravel/NewTravel";
+
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+
+    NewTravel
+    const handleNewTravelClick = () => {
+        console.log('New travel button clicked!');
+    };
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <NewTravel onClick={handleNewTravelClick} />
+        </div>
+    );
 };
 
 export default Home;
