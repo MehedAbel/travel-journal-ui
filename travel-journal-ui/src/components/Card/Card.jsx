07cd4cd55618ce724ee4111e81adaed871470249
Janina <coceijanina@yahoo.com>
2024-03-13 00:35:53 +0200

    Implement card list component

--- travel-journal-ui/src/components/Card/Card.jsx
+++ travel-journal-ui/src/components/Card/Card.jsx
@@ -3,22 +3,23 @@ import './index.css'
 import { MdModeEdit } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
-const Card = () => {
+const Card = ({card}) => {
     return (
         <div className={"card shadow bg-white rounded-4 border-1 border-dark"}>
             <div className="card-body">
                 <div className={"custom-container"}>
+                    {/*todo: replace 'travel' with card.Image after the backend part is done*/}
                     <img src={travel} className={"rounded-4 border-1 border-dark"} alt={"travel-image"}></img>
                 </div>
                 <div className={"d-flex flex-column mt-4 mb-2"}>
-                    <p className={"fs-3"}>Athens</p>
+                    <p className={"fs-3"}>{card.city}</p>
                     <div className={"color-gray"}>
-                        <p>18 Aug 2024</p>
+                        <p>{card.travelDate}</p>
                         <div className={"d-flex flex-row"}>
-                            <p>100 lei</p>
+                            <p>{card.price} {card.currency}</p>
                             <ul>
                                 <li>
-                                    <p>3 Notes</p>
+                                    <p>{card.noNotes} Notes</p>
                                 </li>
                             </ul>
                         </div>
@@ -31,8 +32,6 @@ const Card = () => {
                     <button className={"btn w-50 btn-secondary color-button"}>
                         <RiDeleteBin6Line style={{color: "black"}}/>
                     </button>
-
-
                 </div>
             </div>
         </div>
