00ada2faf1032def30838496d351f6bdff38b282
Janina <coceijanina@yahoo.com>
2024-03-19 18:43:22 +0200

    replace bootstrap-icons with svg

--- travel-journal-ui/src/components/Card/Card.jsx
+++ travel-journal-ui/src/components/Card/Card.jsx
@@ -1,18 +1,19 @@
 import './index.css'
+import deleteIcon from "../../assets/deleteIcon.svg"
+import editIcon from "../../assets/editIcon.svg";
 
 const Card = ({card}) => {
     return (
-        <div className={"card shadow bg-white rounded-4 border-1 border-dark"}>
+        <div className="card shadow bg-white rounded-4 border-1 border-dark">
             <div className="card-body">
-                <div className={"custom-container"}>
-                    {/*todo: replace 'travel' with card.Image after the backend part is done*/}
-                    <img src={card.image} className={"rounded-4 border-1 border-dark"} alt={"travel-image"}></img>
+                <div className="custom-container">
+                    <img src={card.image} className="rounded-4 border-1 border-dark" alt="travel-image"></img>
                 </div>
-                <div className={"d-flex flex-column mt-4 mb-2"}>
-                    <p className={"fs-3"}>{card.city}</p>
-                    <div className={"color-gray"}>
+                <div className="d-flex flex-column mt-4 mb-2">
+                    <p className="fs-3">{card.city}</p>
+                    <div className="color-gray">
                         <p>{card.travelDate}</p>
-                        <div className={"d-flex flex-row"}>
+                        <div className="d-flex flex-row">
                             <p>{card.price} {card.currency}</p>
                             <ul>
                                 <li>
@@ -23,11 +24,11 @@ const Card = ({card}) => {
                     </div>
                 </div>
                 <div className="d-flex flex-row justify-content-between">
-                    <button className={"btn w-50 btn-secondary color-button"}>
-                        <i className={"bi bi-pencil-fill"}></i>
+                    <button className="btn button-container">
+                        <img src={editIcon} alt="edit"></img>
                     </button>
-                    <button className={"btn w-50 btn-secondary color-button"}>
-                        <i className={"bi bi-trash"}></i>
+                    <button className="btn button-container">
+                        <img src={deleteIcon} alt="delete"></img>
                     </button>
                 </div>
             </div>
