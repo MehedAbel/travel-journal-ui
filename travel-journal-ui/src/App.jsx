f49bab80b77e7cb5b8e1669b479f0b02ec90f8fa
Janina <coceijanina@yahoo.com>
2024-03-24 21:17:30 +0200

    Register style

--- travel-journal-ui/src/App.jsx
+++ travel-journal-ui/src/App.jsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
+import {useContext} from "react";
 
-import { Routes, Route, Navigate } from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 
-import { AuthContext } from "./context/AuthContext";
+import {AuthContext} from "./context/AuthContext";
 
 import Home from "./components/Home/Home";
 import Login from "./components/Auth/Login/Login";
@@ -11,27 +11,35 @@ import Register from "./components/Auth/Register/Register";
 import "./App.css";
 import Navbar from "./components/Nav/Navbar";
 import Content from "./components/Content/Content";
+import login_bg from "./assets/login-bg.jpg"
 
 function App() {
-  const { isAuthenticated } = useContext(AuthContext);
+    const {isAuthenticated} = useContext(AuthContext);
+    return (
+        <div style={{
+            backgroundImage: `url(${!isAuthenticated ? login_bg: ""})`,
+            backgroundSize: "1540px 700px",
+            backgroundRepeat: "no-repeat",
+            height:"100%"
+        }}>
+            <div className="container">
+                {isAuthenticated && <Navbar/>}
 
-  return (
-    <>
-      {isAuthenticated && <Navbar />}
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route element={<Content />}>
-            <Route
-                path="/"
-                element={
-                    isAuthenticated ? <Home /> : <Navigate to="/login" />
-                }
-            />
-        </Route>
-      </Routes>
-    </>
-  );
+                <Routes>
+                    <Route path="/login" element={<Login/>}></Route>
+                    <Route path="/register" element={<Register/>}></Route>
+                    <Route element={<Content/>}>
+                        <Route
+                            path="/"
+                            element={
+                                isAuthenticated ? <Home/> : <Navigate to="/login"/>
+                            }
+                        />
+                    </Route>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- travel-journal-ui/src/components/Auth/Register/Register.jsx
+++ travel-journal-ui/src/components/Auth/Register/Register.jsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import {useState, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
-import sha256 from "js-sha256";
-
-import { API_URL } from "../../../config";
 import "./index.css";
+import logo from "../../../assets/Logo-White.svg";
 
 const Register = () => {
     const navigate = useNavigate();
@@ -103,75 +101,99 @@ const Register = () => {
 
 
     return (
-        <form onSubmit={submit} className="register-form">
-            <div className="input-field">
-                <label htmlFor="first-name">First Name</label>
-                <input
-                    type="text"
-                    onChange={(e) => setFirstName(e.target.value)}
-                    placeholder="John"
-                    value={firstName}
-                    id="first-name"
-                />
-                {errors.firstName && <p>{errors.firstName}</p>}
-            </div>
+        <>
+            <div className="d-flex flex-row justify-content-between align-items-center text-white pt-5">
+                <div className="d-flex flex-column fs-3 w-50 text-shadow">
+                    <p className="opacity-75">Welcome to</p>
+                    <img src={logo} alt="logo" className="logo"/>
+                    <ul className="pt-5">
+                        <li> Plan your trips easily.</li>
+                        <li> Have your notes in one place.</li>
+                        <li> Log your trip expenses.</li>
+                    </ul>
+                </div>
+                <div className="card rounded-4 border-1 border-black shadow" style={{width: "40%"}}>
+                    <div className="card-body">
+                        <div className="d-flex justify-content-center m-3">
+                            <span className="card-title fs-3 font-weight-bold">Register Your Account</span>
+                        </div>
+                        <form onSubmit={submit} className="d-flex flex-column gap-2 align-items-center">
+                            <div className="input-field">
+                                <label htmlFor="first-name">First Name</label>
+                                <input
+                                    className="border border-dark rounded-3"
+                                    type="text"
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                    placeholder="John"
+                                    value={firstName}
+                                    id="first-name"
+                                />
+                                {errors.firstName && <p className="badge error-message">{errors.firstName}</p>}
+                            </div>
 
-            <div className="input-field">
-                <label htmlFor="last-name">Last Name</label>
-                <input
-                    type="text"
-                    onChange={(e) => setLastName(e.target.value)}
-                    placeholder="Doe"
-                    value={lastName}
-                    id="last-name"
-                />
-                {errors.lastName && <p>{errors.lastName}</p>}
-            </div>
+                            <div className="input-field">
+                                <label htmlFor="last-name">Last Name</label>
+                                <input
+                                    className="border border-dark rounded-3"
+                                    type="text"
+                                    onChange={(e) => setLastName(e.target.value)}
+                                    placeholder="Doe"
+                                    value={lastName}
+                                    id="last-name"
+                                />
+                                {errors.lastName && <p className="badge error-message">{errors.lastName}</p>}
+                            </div>
 
-            <div className="input-field">
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="john.doe@domain.com"
-                    value={email}
-                    id="email"
-                />
-                {errors.email && <p>{errors.email}</p>}
-            </div>
+                            <div className="input-field">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    className="border border-dark rounded-3"
+                                    type="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="john.doe@domain.com"
+                                    value={email}
+                                    id="email"
+                                />
+                                {errors.email && <p className="badge error-message">{errors.email}</p>}
+                            </div>
 
-            <div className="input-field">
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Type in your password"
-                    value={password}
-                    id="password"
-                />
-                {errors.password && <p>{errors.password}</p>}
-            </div>
+                            <div className="input-field">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    className="border border-dark rounded-3"
+                                    type="password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Type in your password"
+                                    value={password}
+                                    id="password"
+                                />
+                                {errors.password && <p className="badge error-message">{errors.password}</p>}
+                            </div>
 
-            <div className="input-field">
-                <label htmlFor="confirm-password">Confirm Password</label>
-                <input
-                    type="password"
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    placeholder="Retype in your password"
-                    value={confirmPassword}
-                    id="confirm-password"
-                />
-                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-            </div>
+                            <div className="input-field">
+                                <label htmlFor="confirm-password">Confirm Password</label>
+                                <input
+                                    className="border border-dark rounded-3"
+                                    type="password"
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    placeholder="Retype in your password"
+                                    value={confirmPassword}
+                                    id="confirm-password"
+                                />
+                                {errors.confirmPassword && <p className="badge error-message">{errors.confirmPassword}</p>}
+                            </div>
 
-            <button type="submit" className="submit-btn">
-                Register
-            </button>
-            <span onClick={() => navigate("/login")} className="login-link">
-                Already have an account?
-            </span>
-        </form>
-    );
+                            <button type="submit" className="btn btn-dark rounded-3 w-100">
+                                Register
+                            </button>
+                            <span>Do you have an account? <a href="/login" className="login-link">
+                Log In
+            </a></span>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>);
 };
 
 export default Register;
