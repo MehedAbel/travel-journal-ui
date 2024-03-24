2f475a9d53be68ff7e1379ede4b35dae934e398b
Claudita8 <claudiaolaru2002@gmail.com>
2024-03-24 14:00:22 +0200

    travel-details page

---
+++ travel-journal-ui/src/components/TravelDetails/TravelDetails.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './index.css';
+import placeholder from "../../assets/placeholderImage.svg";
+
+const TravelDetails = () => {
+
+    const mockTravelDetails = {
+        id: "1",
+        image: placeholder,
+        city: 'Athens',
+        startTravelDate: '18 Aug 2023',
+        endTravelDate: '20 Aug 2023',
+        notes: '3 notes',
+        description: 'Stepping into Athens felt like stepping into history itself. The Acropolis, with its Parthenon standing tall, whispered tales of ancient glory. Plaka\'s alleys, adorned with bougainvillaeas, led me to delectable spanakopita. The National Archaeological Museum brought gods and heroes to life through sculptures and relics. Monastiraki Square buzzed with art and energy, a vibrant heart. Dinner at a taverna filled me with moussaka and music. Now, gazing at the illuminated Acropolis from my Airbnb, I\'m grateful for this plunge into Athens\' captivating blend of old and new.'
+    };
+
+    mockTravelDetails.travelDate = `${mockTravelDetails.startTravelDate} to ${mockTravelDetails.endTravelDate}`;
+
+    return (
+        <div className="travel-details">
+            <div className="header">
+                <div className="city">{mockTravelDetails.city}</div>
+                <div className="details">
+                    <div className="dates">{mockTravelDetails.travelDate}</div>
+                    <div className="date-separator">•</div>
+                    <div className="notes">{mockTravelDetails.notes}</div>
+                </div>
+            </div>
+
+            <div>
+                <img src={mockTravelDetails.image} alt="placeholder" />
+            </div>
+
+            <div className="description">
+                <p>{mockTravelDetails.description}</p>
+            </div>
+
+            <div className="notes-container">
+                <div className="button-wrapper">
+                    <button className="new-note-button"> + New Note</button>
+                </div>
+                <div className="notes-table">
+                    <div>No notes available yet.</div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default TravelDetails;
