034579b033240cbe69f76943b53c59fdf8344b06
Claudiu <baiceaclaudiuadrian@gmail.com>
2024-03-26 20:07:24 +0200

    Made classes use BEM naming convention and finished login card css

--- travel-journal-ui/src/components/Auth/Login/Login.jsx
+++ travel-journal-ui/src/components/Auth/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+﻿import { useContext, useState } from "react";
 
 import sha256 from "js-sha256";
 
@@ -41,50 +41,59 @@ const Login = () => {
   };
 
   return (
-      <div className="flex-wrapper">
-          <div className="flex-row">
-              <div className="flex-item">
-                  <div className="login-description">
-                          <h3>Welcome to</h3>
-                          <img className="logo" src = "src/assets/TravelJournal.svg"/>
-                          <ul className="description-list">
-                              <li>Plan your trips easily.</li>
-                              <li>Have your notes in one place.</li>
-                              <li>Log your trip expenses.</li>
-                          </ul>
+    <div id="login">
+      <div className="login-row">
+        <div className="login-description">
+          <h3 className="login-description__subtitle">Welcome to</h3>
+          <img
+            className="login-description__logo"
+            src="src/assets/TravelJournal.svg"
+          />
+          <ul className="login-description__list">
+            <li>Plan your trips easily.</li>
+            <li>Have your notes in one place.</li>
+            <li>Log your trip expenses.</li>
+          </ul>
+        </div>
+        <div className="login-card">
+          <h3 className="login-card__title">Log Into Your Account</h3>
+          <form onSubmit={submit} id="login-form" className="login-form">
+            <div className="login-form__input-field">
+              <label>Email</label>
+              <input
+                type="email"
+                placeholder="john.doe@domain.com"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
 
-                  </div>
-              </div>
-              <div className="flex-item">
-                  <form onSubmit={submit} className="login-form">
-                      <div className="login-info">
-                          <h3>Log Into Your Account</h3>
-                      </div>
-                      <div className="input-field">
-                          <label>Email</label>
-                          <input type="email" placeholder="john.doe@domain.com"
-                                 onChange={(e) => setEmail(e.target.value)}/>
-                      </div>
+            <div className="login-form__input-field">
+              <label>Password</label>
+              <input
+                type="password"
+                placeholder="Type in your password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </form>
 
-                      <div className="input-field">
-                          <label>Password</label>
-                          <input type="password" placeholder="Type in your password"
-                                 onChange={(e) => setPassword(e.target.value)}/>
-                      </div>
+          <button type="submit" form="login-form" className="login-form__submit-btn">
+            <h3>Log In</h3>
+          </button>
 
-                      <button type="submit" className="submit-btn">
-                          Log In
-                      </button>
-                      <span onClick={() => navigate("/register")} className="register-link">
-          <button>You can create an account here{" "}</button>
-      </span>
-                  </form>
-              </div>
-
-          </div>
-
+          {
+            /*
+            <span
+              onClick={() => navigate("/register")}
+              className="login-form__register-link"
+            >
+              <button>You can create an account here{" "}</button>
+            </span>
+            */
+          }
+        </div>
       </div>
-
+    </div>
   );
 };
 
