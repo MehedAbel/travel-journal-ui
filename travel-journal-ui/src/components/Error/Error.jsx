06724d5ec4190cfbb4a2ebfdf120a0b9caeec048
Abel Mehedinteanu <111386807+abel325@users.noreply.github.com>
2024-04-01 22:43:35 +0300

    add ariaLive to Error component props

--- travel-journal-ui/src/components/Error/Error.jsx
+++ travel-journal-ui/src/components/Error/Error.jsx
@@ -1,8 +1,8 @@
 import './index.css'
 
-const Error = ({ errorMessage, id = "", style = {} }) => {
+const Error = ({ errorMessage, id = "", style = {}, ariaLive = "polite"}) => {
     return (
-        <div className="w-100" aria-live="polite">
+        <div className="w-100" aria-live={ariaLive}>
             <p className="badge text-wrap error-message" id={id} style={style}>{errorMessage}</p>
         </div>
     );
