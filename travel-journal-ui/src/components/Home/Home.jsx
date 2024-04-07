4b3c04fb41da58d392f4d4345ce2630c654e45eb
Claudita8 <claudiaolaru2002@gmail.com>
2024-04-07 20:50:42 +0300

    Implement dynamic data fetching from database for Home component

--- travel-journal-ui/src/components/Home/Home.jsx
+++ travel-journal-ui/src/components/Home/Home.jsx
@@ -1,34 +1,70 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
 import CardList from "../CardList/CardList.jsx";
 import NewTravel from "../NewTravel/NewTravel.jsx";
-import travelImage from '../../assets/login-bg.jpg';
+import {API_URL} from "../../config.js";
 
 const Home = () => {
-    /*NewTravel*/
-    const handleNewTravelClick = () => {
-        console.log('New travel button clicked!');
+    const [entities, setEntities] = useState([]);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        const tokenType = localStorage.getItem("tokenType");
+
+        fetch(`${API_URL}/travel-journal/my-travels`, {
+            method: "GET",
+            headers: {
+                Authorization: `${tokenType} ${token}`,
+                "Content-Type": "application/json",
+            }
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data) {
+                    setEntities(data);
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }, []);
+
+    const formatDate = (dateArray) => {
+        const [year, month, day] = dateArray;
+        return `${day}/${month}/${year}`;
     };
 
-    // todo: call api to get entitiesMock
-    let entitiesMock = [
-        {id: "1", image:travelImage, city: "Craiova", travelDate: "miercuri, 13 martie 2024", noNotes: "6", price: "200", currency: "lei"},
-        {id: "2", image:travelImage, city: "Pitesti", travelDate: "miercuri, 13 martie 2024", noNotes: "12", price: "245", currency: "lei"},
-        {id: "3", image:travelImage, city: "Bucuresti", travelDate: "miercuri, 13 martie 2024", noNotes: "2", price: "12", currency: "lei"},
-        {id: "4", image:travelImage, city: "Orsova", travelDate: "miercuri, 13 martie 2024", noNotes: "5", price: "100", currency: "lei"},
-        {id: "5", image:travelImage, city: "Iasi", travelDate: "miercuri, 13 martie 2024", noNotes: "4", price: "200", currency: "lei"},
-        {id: "6", image:travelImage, city: "Timisoara", travelDate: "miercuri, 13 martie 2024", noNotes: "1", price: "400", currency: "lei"},
-        {id: "7", image:travelImage, city: "Sibiu", travelDate: "miercuri, 13 martie 2024", noNotes: "2", price: "564", currency: "lei"},
-        {id: "8", image:travelImage, city: "CLuj-Napoca", travelDate: "miercuri, 13 martie 2024", noNotes: "3", price: "1000", currency: "lei"}
-    ];
+    let entitiesStorage = [];
+    if (entities.length > 0) {
+        entities.forEach(item => {
+            entitiesStorage.push({
+                id: item.travelId,
+                image: item.coverPhoto.fileContent,
+                city: item.location,
+                startDate: formatDate(item.startDate),
+                endDate: formatDate(item.endDate),
+                price: item.budget,
+                noNotes: item.notesNumber ? item.notesNumber : 0,
+                currency: "lei"
+            });
+        });
+    }
+
 
     return (
         <div>
             <h1>Home Page</h1>
-            <Breadcrumbs />
-            <CardList entities={entitiesMock}></CardList>
-            <NewTravel onClick={handleNewTravelClick} />
+            <Breadcrumbs/>
+            {entities.length > 0 ? (
+                <CardList entities={entitiesStorage} />
+            ) : (
+                <p>Nu aveți niciun travel</p>
+            )}
+
+            <NewTravel/>
+
         </div>
+
     );
 };
 
