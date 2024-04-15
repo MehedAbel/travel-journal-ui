3d2d7e5b0140c9c2500ff401bb06e1d0fb0f2f1a
Claudita8 <claudiaolaru2002@gmail.com>
2024-04-15 21:04:43 +0300

    Removed h1 tag

--- travel-journal-ui/src/components/Home/Home.jsx
+++ travel-journal-ui/src/components/Home/Home.jsx
@@ -47,7 +47,6 @@ const Home = () => {
 
     return (
         <div>
-            <h1>Home Page</h1>
             <Breadcrumbs />
             {entities.length > 0 ? (
                 <CardList entities={entities} onEdit={(card) => setSelectedCard(card)} />
