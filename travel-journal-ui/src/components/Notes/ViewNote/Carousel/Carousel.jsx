f404bd09b4bd53da17e35829ab7398b8f776c7f9
Abel Mehedinteanu <111386807+abel325@users.noreply.github.com>
2024-04-24 23:31:05 +0300

    make carousel

---
+++ travel-journal-ui/src/components/Notes/ViewNote/Carousel/Carousel.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+import './Carousel.css'; // don't use css modules here, this modifies the styles of the react-image-gallery library
+import ArrowLeft from '../../../../assets/arrow-left.svg';
+import ArrowRight from '../../../../assets/arrow-right.svg';
+
+const Carousel = ({ images }) => {
+    const displayedImages = images.slice(0, 7);
+
+    const imageUrls = displayedImages.map((imageUrl, index) => ({
+        original: imageUrl,
+        thumbnail: imageUrl,
+        originalAlt: `Image ${index + 1}`,
+        thumbnailAlt: `Image ${index + 1}`
+    }));
+
+    const renderLeftNav = (onClick, disabled) => {
+        return (
+            <button
+                type="button"
+                className="image-gallery-custom-left-nav"
+                disabled={disabled}
+                onClick={onClick}>
+                <img src={ArrowLeft} alt="Left Arrow" />
+            </button>
+        );
+    };
+
+    const renderRightNav = (onClick, disabled) => {
+        return (
+            <button
+                type="button"
+                className="image-gallery-custom-right-nav"
+                disabled={disabled}
+                onClick={onClick}>
+                <img src={ArrowRight} alt="Right Arrow" />
+            </button>
+        );
+    };
+
+    return (
+        <ImageGallery
+            items={imageUrls}
+            renderLeftNav={renderLeftNav}
+            renderRightNav={renderRightNav}
+            showFullscreenButton={false}
+            showPlayButton={false}
+            disableSwipe={false}
+            showThumbnails={imageUrls.length > 1 ? true : false}
+        />
+    );
+};
+
+export default Carousel;
